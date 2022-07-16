525fba0bb394c077e109f8221b3e1b2157fc57a3
Alexis R <64582375+alexisradice@users.noreply.github.com>
2022-07-16 20:00:45 +0200

    Add files via upload

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,18 +6,47 @@
 
 # include <stdlib.h>
 
-typedef struct s_map
+typedef struct s_data_all
 {
 	char		**map;
 	int			size_line;
 	int			count_line;
-}	t_map;
 
-typedef struct s_mlx
-{
-	void	*mlx_ptr;
-	void	*window_ptr;
-	t_map	*map;
-}	t_mlx;
+	void		*mlx_ptr;
+	void		*window_ptr;
+	int			window_width;
+	int			window_height;
+
+	int			size_asset;
+
+	void		*player_top;
+	void		*player_left;
+	void		*player_back;
+	void		*player_right;
+	void		*ground;
+	void		*obstacle;
+	void		*collectable;
+	void		*exit;
+
+
+	char		*map_destination;
+
+	int			player_x;
+	int			player_y;
+
+
+	int			collectible_count;
+	int			collectible_size;
+
+	int			player_spawn_point_count;
+	int			exit_count;
+
+	int			player_move_count;
+
+	char		current_direction;
+	char		*direction_path;
+
+
+}	t_data_all;
 
 #endif
